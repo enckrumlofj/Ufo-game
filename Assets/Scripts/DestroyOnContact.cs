bd8eeb59a686425dd526edaac55da52a1ed11ce7
enckrumlofj <alexxspam@yandex.ru>
2016-09-04 15:50:24 +0700

    доработка существующего и добавление нового
    исправлен прилет нло
    добавлено ускорение при нажатии на мусор
    изменено регулирование спауна бомб
    уменьшено колво жизней
    деактивируются нло после окончания игры

--- Assets/Scripts/DestroyOnContact.cs
+++ Assets/Scripts/DestroyOnContact.cs
@@ -3,12 +3,16 @@ using System.Collections;
 
 public class DestroyOnContact : MonoBehaviour {
 
-	public static int lives=30;
+	public static int lives=3;
 	public GameObject explosion;
 	public GameObject ufo;
 	public float respawnTime;
 
 	private Vector3 pos;
+
+	void Start(){
+		lives = 3;
+	}
 
 	void OnTriggerEnter2D(Collider2D other){
 
@@ -19,21 +23,19 @@ public class DestroyOnContact : MonoBehaviour {
 			);
 			Destroy (other.gameObject);
 			transform.parent.gameObject.SetActive(false);
-		//	this.GetComponent<Animator>().Play ("ufo");
 			GameObject cloneExplosion=(GameObject)Instantiate (explosion, transform.position, transform.rotation);
 			Destroy (cloneExplosion, 0.5f);
 			Invoke("Respawn",respawnTime);
 		}else if (other.gameObject.CompareTag ("cow")) {
 			lives--;
 		}
+		else if (other.gameObject.CompareTag ("garbage")) {
+		}
 		Destroy (other.gameObject);
 
 	}
 	void Respawn(){
-		//GameObject cloneUFOComing=(GameObject)Instantiate (ufoCome, transform.position, transform.rotation);
-		//Destroy (cloneUFOComing, 0.5f);
 		this.GetComponent<Animator>().Play ("ufo");
-		//this.GetComponent<Animator> ().Stop ();
 		transform.parent.gameObject.SetActive (true);
 		transform.parent.FindChild("light").gameObject.SetActive (false);
 		Invoke ("activateLight", 2f);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class GameController : MonoBehaviour {
 	public Camera cam;
 	public GameObject[] spawnObjects;
+	public float[] probabilities;
 	public float spawnTime;
 	public Text scoreCows;
 	public Text scoreGarbage;
@@ -14,14 +15,13 @@ public class GameController : MonoBehaviour {
 	public GameObject gameOverTextC;
 	public Text gameOverText;
 	public GameObject exitButton;
+	public GameObject[] lights;
 
 	private int scoreCowsCounter;
 	private int scoreGarbageCounter;
 	private float maxWidth;
 	private float timeLeftCounter;
 	private bool playing;
-
-	//private Collider2D collider1;
 
 	Vector3 touchPosWorld;
 
@@ -45,9 +45,7 @@ public class GameController : MonoBehaviour {
 		if (Input.touchCount > 0 && Input.GetTouch(0).phase == touchPhase) {
 			//We transform the touch position into word space from screen space and store it.
 			touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-
 			Vector2 touchPosWorld2D = new Vector2(touchPosWorld.x, touchPosWorld.y);
-
 			//We now raycast with this information. If we have hit something we can process it.
 			RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
 
@@ -62,23 +60,26 @@ public class GameController : MonoBehaviour {
 					scoreGarbageCounter++;
 					scoreGarbage.text = "Garbage:\n" + scoreGarbageCounter;
 					Destroy(touchedObject);
+					//multiply speed of lights 2 times if u cathed garbage
+					for (int i = 0; i < lights.Length; i++) {
+						(lights [i].GetComponent (typeof(AreaEffector2D)) as AreaEffector2D).forceMagnitude = 1.3f*(lights [i].GetComponent (typeof(AreaEffector2D)) as AreaEffector2D).forceMagnitude;
+					}
+					//
 				}
-					
-				//touchedObject should be the object someone touched.
-
-				//Debug.Log("Touched " + touchedObject.transform.name);
 			}
 		}
 	}
 
 
 	void Start(){
+		
 		playing = false;
+		toggleUfos (false);
 		scoreCows.text = "Cows saved:\n0";
 		scoreGarbage.text = "Garbage:\n0 ";
 		scoreCowsCounter = 0;
 		scoreGarbageCounter = 0;
-		timeLeftCounter = 30.0f;
+		timeLeftCounter = 40.0f;
 		if (cam == null) {
 			cam = Camera.main;
 		}
@@ -94,13 +95,21 @@ public class GameController : MonoBehaviour {
 		startButton.SetActive (false);
 		exitButton.SetActive (false);
 		playing = true;
+		toggleUfos (true);
 		StartCoroutine (Spawn ());
 	}
 
+	void toggleUfos(bool t){
+		for (int i = 0; i < lights.Length; i++) {
+			lights [i].SetActive (t);
+			lights [i].transform.parent.gameObject.SetActive (t);
+		}
+	}
+
 	IEnumerator Spawn(){
-		//yield return new WaitForSeconds (1f);
+		yield return new WaitForSeconds(2f);
 		while (timeLeftCounter>0) {
-			GameObject spawnObject = spawnObjects [Random.Range (0, spawnObjects.Length)];
+			GameObject spawnObject = spawnObjects [Choose (probabilities)];
 			Vector3 spawnPosition = new Vector3 (
 				                        Random.Range (-maxWidth, maxWidth),
 				                        transform.position.y + spawnObject.GetComponent<Renderer> ().bounds.extents.y,
@@ -110,8 +119,27 @@ public class GameController : MonoBehaviour {
 			Instantiate (spawnObject, spawnPosition, spawnRotation);
 			yield return new WaitForSeconds (spawnTime);
 		}
+		toggleUfos (false);
 		gameOverText.text="Game Over\nScore: "+scoreCowsCounter.ToString ();
 		gameOverTextC.SetActive (true);
 		restartButton.SetActive (true);
+	}
+
+	int Choose(float[] probs){
+		float total = 0;
+		foreach (float elem in probs) {
+			total += elem;
+		}
+
+		float randomPoint = Random.value * total;
+
+		for (int i = 0; i < probs.Length; i++) {
+			if (randomPoint < probs [i]) {
+				return i;
+			}else {
+				randomPoint -= probs[i];
+			}
+		}
+		return -1;//Should never appear, or u got wrong parameters
 	}
 }
